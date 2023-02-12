39ef8bdc906c77e5bf5ab72453c90e223efa7601
melody <vivihl2131@gmail.com>
2023-02-13 03:44:16 +0800

    modify files

--- app.js
+++ app.js
@@ -36,32 +36,55 @@ app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
 
+
 // 處理搜尋頁面的請求和回應
 app.get('/search', (req, res) => {
   // 篩選字串
-  const keyword = req.query.keyword
-  // 篩選餐廳名稱
-  const restaurantName = restaurantList.results.filter(restaurant => {
-    return (restaurant.name.toLowerCase().includes(keyword.toLowerCase()))
-  })
-  // 篩選餐廳類別
-  const restaurantCategory = restaurantList.results.filter(restaurant => {
-    return (restaurant.category.toLowerCase().includes(keyword.toLowerCase()))
-  })
+  const keyword = req.query.keyword.trim().toLowerCase()
 
-  // 合併（篩選名稱、篩選類別）不重複的餐廳
-  const searchSet = new Set()
-  // 新增至Set: 篩選的物件資料會放在陣列內，為了用Set合併要先forEach把物件資料取出，再新增至Set物件
-  restaurantName.forEach(restaurant => { searchSet.add(restaurant) })
-  restaurantCategory.forEach(restaurant => { searchSet.add(restaurant) })
-  // Set物件轉陣列: 先展開元素再放到陣列
-  const searchArr = [...searchSet]
-  // 使用sort的自定義排序方法，將資料的id做排序，畫面上會照順序顯示資料
-  searchArr.sort((a, b) => a.id - b.id)
+  // 篩選餐廳名稱、類別
+  const restaurantFiltered = restaurantList.results.filter(restaurant =>
+    restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
+  )
+
+  // 當未輸入或符合條件筆數為0，會回到首頁
+  if (req.query.keyword.length === 0 || restaurantFiltered.length === 0) {
+    return res.redirect("/")
+  }
 
   // 渲染局部樣板
-  res.render('index', { restaurants: searchArr, keyword: keyword })
+  res.render('index', { restaurants: restaurantFiltered, keyword: keyword })
 })
+
+// 方法二
+// 處理搜尋頁面的請求和回應
+// app.get('/search', (req, res) => {
+//   // 篩選字串
+//   const keyword = req.query.keyword.trim().toLowerCase()
+
+//   // 篩選餐廳名稱
+//   const restaurantName = restaurantList.results.filter(restaurant => {
+//     return (restaurant.name.toLowerCase().includes(keyword))
+//   })
+//   // 篩選餐廳類別
+//   const restaurantCategory = restaurantList.results.filter(restaurant => {
+//     return (restaurant.category.toLowerCase().includes(keyword))
+//   })
+
+//   // 合併（篩選名稱、篩選類別）不重複的餐廳
+//   const searchSet = new Set()
+//   // 新增至Set: 篩選的物件資料會放在陣列內，為了用Set合併要先forEach把物件資料取出，再新增至Set物件
+//   restaurantName.forEach(restaurant => { searchSet.add(restaurant) })
+//   restaurantCategory.forEach(restaurant => { searchSet.add(restaurant) })
+//   // Set物件轉陣列: 先展開元素再放到陣列
+//   const searchArr = [...searchSet]
+//   // 使用sort的自定義排序方法，將資料的id做排序，畫面上會照順序顯示資料
+//   searchArr.sort((a, b) => a.id - b.id)
+
+//   // 渲染局部樣板
+//   res.render('index', { restaurants: searchArr, keyword: keyword })
+// })
+
 
 // 處理 show 個別資料頁面的請求和回應
 app.get('/restaurants/:restaurant_id', (req, res) => {
